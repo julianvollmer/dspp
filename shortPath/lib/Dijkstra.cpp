c3745006f16c2a692e0708649a76a2f0b99d01f1
Julian Vollmer <julianvollmer@gmail.com>
2013-04-26 22:44:21 +0200

    Dijsktra angefangen !

--- shortPath/lib/Dijkstra.cpp
+++ shortPath/lib/Dijkstra.cpp
@@ -5,3 +5,40 @@ Dijkstra::Dijkstra(){};
 void Dijkstra::test_funktion(){	
 	cout << "alksdma";
 }
+
+
+// http://www.cprogramming.com/tutorial/computersciencetheory/dijkstra.html
+// Given a graph, G, with edges E of the form (v1, v2) and vertices V, and a
+// source vertex, s
+
+// dist : array of distances from the source to each vertex
+// prev : array of pointers to preceding vertices
+// i    : loop index
+// F    : list of finished vertices
+// U    : list or heap unfinished vertices
+
+// /* Initialization: set every distance to INFINITY until we discover a path */
+// for i = 0 to |V| - 1
+//     dist[i] = INFINITY
+//     prev[i] = NULL
+// end
+
+// /* The distance from the source to the source is defined to be zero */
+// dist[s] = 0
+
+//  This loop corresponds to sending out the explorers walking the paths, where
+//  * the step of picking "the vertex, v, with the shortest path to s" corresponds
+//  * to an explorer arriving at an unexplored vertex 
+
+// while(F is missing a vertex)
+//    pick the vertex, v, in U with the shortest path to s
+//    add v to F
+//    for each edge of v, (v1, v2)
+//         /* The next step is sometimes given the confusing name "relaxation"
+//         if(dist[v1] + length(v1, v2) < dist[v2])
+//             dist[v2] = dist[v1] + length(v1, v2)
+//             prev[v2] = v1
+//             possibly update U, depending on implementation
+//         end if
+//     end for
+// end while
