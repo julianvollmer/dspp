885927efe0cefe14f68580dda5ba105418b5a52e
Julian Vollmer <julianvollmer@gmail.com>
2013-05-16 12:35:47 +0200

    kA was hier nicht commited ist

--- shortPath/lib/Dijkstra.cpp
+++ shortPath/lib/Dijkstra.cpp
@@ -17,12 +17,12 @@ void Dijkstra::test_boost(){
 
     //An edge is just a connection between two vertitices. Our verticies above
     //are an enum, and are just used as integers, so our edges just become
-    //a std::pair<int, int>
-    typedef std::pair<int, int> Edge;
+    //a pair<int, int>
+    typedef pair<int, int> Edge;
 
     //Example uses an array, but we can easily use another container type
     //to hold our edges. 
-    std::vector<Edge> edgeVec;
+    vector<Edge> edgeVec;
     edgeVec.push_back(Edge(A,B));
     edgeVec.push_back(Edge(A,D));
     edgeVec.push_back(Edge(C,A));
@@ -34,37 +34,38 @@ void Dijkstra::test_boost(){
     //Now we can initialize our graph using iterators from our above vector
     UndirectedGraph g(edgeVec.begin(), edgeVec.end(), N);
 
-    std::cout << num_edges(g) << "\n";
+    cout << num_edges(g) << "\n";
 
     //Ok, we want to see that all our edges are now contained in the graph
     typedef boost::graph_traits<UndirectedGraph>::edge_iterator edge_iterator;
 
     //Tried to make this section more clear, instead of using tie, keeping all
     //the original types so it's more clear what is going on
-    std::pair<edge_iterator, edge_iterator> ei = edges(g);
+    pair<edge_iterator, edge_iterator> ei = edges(g);
+    
     for(edge_iterator edge_iter = ei.first; edge_iter != ei.second; ++edge_iter) {
-        std::cout << "(" << source(*edge_iter, g) << ", " << target(*edge_iter, g) << ")\n";
+        cout << "(" << source(*edge_iter, g) << ", asda" << target(*edge_iter, g) << ")\n";
     }
 
-    std::cout << "\n";
+    cout << "\n";
     //Want to add another edge between (A,E)?
     add_edge(A, E, g);
 
     //Print out the edge list again to see that it has been added
     for(edge_iterator edge_iter = ei.first; edge_iter != ei.second; ++edge_iter) {
-        std::cout << "(" << source(*edge_iter, g) << ", " << target(*edge_iter, g) << ")\n";
+        cout << "( A" << source(*edge_iter, g) << ", " << target(*edge_iter, g) << ")\n";
     }
 
     //Finally lets add a new vertex - remember the verticies are just of type int
     int F = add_vertex(g);
-    std::cout << F << "\n";
+    cout << F << "\n";
 
     //Connect our new vertex with an edge to A...
     add_edge(A, F, g);
 
     //...and print out our edge set once more to see that it was added
     for(edge_iterator edge_iter = ei.first; edge_iter != ei.second; ++edge_iter) {
-        std::cout << "(" << source(*edge_iter, g) << ", " << target(*edge_iter, g) << ")\n";
+        cout << "(" << source(*edge_iter, g) << ", " << target(*edge_iter, g) << ")\n";
     }
     
 }
